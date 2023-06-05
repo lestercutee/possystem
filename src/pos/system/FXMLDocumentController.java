6926117c3102564c84a006c54d5c3f394bce3d28
lestercutee <dagansinalester09@gmail.com>
2023-06-06 05:07:02 +0800

    POS System Project

---
+++ src/pos/system/FXMLDocumentController.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package pos.system;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+/**
+ * FXML Controller class
+ *
+ * @author Admin
+ */
+public class FXMLDocumentController implements Initializable {
+
+    @FXML
+    private Label q1;
+    @FXML
+    private Label q2;
+    @FXML
+    private Label q3;
+    @FXML
+    private Label q4;
+    @FXML
+    private Label q5;
+    @FXML
+    private Label q6;
+    @FXML
+    private Label q7;
+    @FXML
+    private Label q8;
+    @FXML
+    private Label q9;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
---
+++ src/pos/system/POSSystem.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMain.java to edit this template
+ */
+package pos.system;
+
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Admin
+ */
+public class POSSystem extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("fxmldocument.css").toExternalForm());
+        stage.setScene(scene);
+        stage.show();
+        
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
